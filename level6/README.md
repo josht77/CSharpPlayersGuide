ce17e5919fa725687fa25993697f4e38a0c239ab
Josh Thomas <jthomas2023@chadwickschool.org>
2022-07-05 11:08:15 -0700

    docs: Add level6

---
+++ level6/README.md
@@ -0,0 +1,17 @@
+# Questions 
+
+* How do you run a specfic cs file when you have two in the same folder?
+* how to fix josh terminal within VS code because it does not stay open?
+* why bool not boolean? 
+
+# Knowledge Check
+
+* 1) False, the int type cannot store any integer
+* 2) byte, short, int, long 
+* 3) false (signed means you allow positive and negatives)
+* 4) uint stores higher numbers
+* 5) float, double, decimal 
+* 6) double
+* 7) decimal
+* 8) string (always lowercase)
+* 9) bool
---
+++ level6/TheVariableShop.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+byte b = 7;
+sbyte sb = 32;
+short sh = 111;
+ushort u = 11;
+long large = 2348936453;
+ulong largerNum = 1000000000000;
+int n = 5;
+uint un = 100000;
+char c = 'a';
+string s = "Josh";
+double d = 3.145;
+float f = 3.7f;
+decimal de = 2.1m;
+bool hasMoney = false;
+
+b = 8;
+sb = 33;
+sh = 1112;
+u = 111;
+large = 22222;
+largerNum = 1000300000;
+n = 5;
+un = 1000;
+c = 'b';
+s = "Carter";
+d = 6.69;
+f = 5.4f;
+de = 1.5m;
+hasMoney = true;
+
+Console.WriteLine($"{b}");
+Console.WriteLine($"{sb}");
+Console.WriteLine($"{sh}");
+Console.WriteLine($"{u}");
+Console.WriteLine($"{large}");
+Console.WriteLine($"{largerNum}");
+Console.WriteLine($"{n}");
+Console.WriteLine($"{un}");
+Console.WriteLine($"{c}");
+Console.WriteLine($"{s}");
+Console.WriteLine($"{d}");
+Console.WriteLine($"{f}");
+Console.WriteLine($"{de}");
+Console.WriteLine($"{hasMoney}");
