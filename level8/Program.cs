d47771aa79ebbebd9f25e4a87751c0cfb67cb374
Josh Thomas <jthomas2023@chadwickschool.org>
2022-07-06 11:01:54 -0700

    docs: Add level8

---
+++ level8/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Title = "Defense of Consolas";
+
+
+Console.WriteLine("What is the target row?: ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What is the target column?: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine($"({row}, {col - 1})");
+Console.WriteLine($"({row - 1}, {col})");
+Console.WriteLine($"({row}, {col + 1})");
+Console.WriteLine($"({row + 1}, {col})");
+
+Console.Beep();
+
---
+++ level8/README.md
@@ -0,0 +1,7 @@
+# Questions 
+
+* 
+
+# Knowledge Check
+
+* 
