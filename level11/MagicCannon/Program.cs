e57fbcdfa5c83061bee77dade8f543af3b8f35d3
Josh Thomas <jthomas2023@chadwickschool.org>
2022-07-06 12:18:21 -0700

    docs: Add level11

---
+++ level11/MagicCannon/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+string blast = " ";
+for (int i = 1; i < 101; i++)
+{
+    if (i % 5 == 0 && i % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        blast = "Elctric and Fire";
+    }
+    else if (i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        blast = "Electric";
+    }
+    else if (i % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        blast = "Fire";
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        blast = "Normal";
+    }
+    Console.WriteLine($"{i}: {blast}");
+}
+
+
---
+++ level11/Prototype/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+do
+{
+    Console.WriteLine("User 1, enter a number between 0 and 100: ");
+    num = Convert.ToInt32(Console.ReadLine()!);
+}
+while(num < 0 || num> 100);
+Console.Clear();
+Console.WriteLine("User 2, guess the number");
+int guess;
+while(true)
+{
+    Console.Write("What is your next guess?: ");
+    guess = Convert.ToInt32(Console.ReadLine()!);
+    if(guess > num) {
+        Console.WriteLine($"{guess} is too high.");
+    } else if (guess < num)
+    {
+        Console.WriteLine($"{guess} is too low.");
+    } else {
+        break;
+    }   
+}
+Console.WriteLine("You guessed the number!");
