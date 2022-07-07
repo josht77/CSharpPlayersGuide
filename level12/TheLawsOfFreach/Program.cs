2ea7f1f00968d7a2a4ee4f1d683d84508e65fcea
Carter Warren <cwarren2023@chadwickschool.org>
2022-07-07 10:45:18 -0700

    docs: Add level12

---
+++ level12/TheLawsOfFreach/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentSmallest = int.MaxValue; // Start higher than anuthing in the array
+foreach (int i in array)
+{
+    if (i < currentSmallest)
+    {
+        currentSmallest = i;
+    }
+}
+Console.WriteLine($"Smallest: {currentSmallest}");
+
+int total = 0;
+foreach (int j in array)
+{
+    total += j;
+}
+float average = (float)total / array.Length;
+Console.WriteLine($"Average: {average}");
---
+++ level12/TheReplicator/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] ogArray = new int[5];
+int[] newArr = new int[5];
+int input;
+
+for(int i = 0; i < 5; i++) {
+    Console.WriteLine("Input a number: ");
+    input = Convert.ToInt32(Console.ReadLine()!);
+    ogArray[i] = input;
+}
+
+for(int i = 0; i < 5; i++) {
+    newArr[i] = ogArray[i];
+}
+for(int i = 0; i < 5; i++){
+    Console.WriteLine($"{ogArray[i]} {newArr[i]}");
+}
+
