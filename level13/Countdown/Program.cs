a49e89ae5284f4817de5cc989a9019b708775f37
Carter Warren <cwarren2023@chadwickschool.org>
2022-07-07 11:09:41 -0700

    docs: Add level 13

---
+++ level13/Countdown/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+countDown(10);
+int countDown(int start) {
+    Console.WriteLine(start);
+    if (start == 1) {
+        return 1;
+    } 
+    return countDown(start - 1);
+}
+
+
---
+++ level13/TakingANumber/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(AskForNumberInRange("Input number between 0 and 100: ", 1, 100));
+
+int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int input = Convert.ToInt32(Console.ReadLine()!);
+    return input;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    Console.WriteLine(text);
+    int input = Convert.ToInt32(Console.ReadLine()!);
+    while (input > max || input < min)
+    {
+        Console.WriteLine(text);
+        input = Convert.ToInt32(Console.ReadLine()!);
+    }
+    return input;
+
+}
