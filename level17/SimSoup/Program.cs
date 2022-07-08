3a479a0054893fec51d4032c6b38b1efbf4b1606
Josh Thomas <jthomas2023@chadwickschool.org>
2022-07-08 12:13:51 -0700

    docs: Add code to level17

---
+++ level17/SimSoup/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+(SoupType St, MainIngredient Mi, Seasoning Se) soup = GetSoup();
+Console.WriteLine($"{soup.Se} {soup.Mi} {soup.St}");
+
+(SoupType, MainIngredient, Seasoning) GetSoup() {
+
+    SoupType St = GetSoupType();
+    MainIngredient Mi = GetMainIngredient();
+    Seasoning Se = GetSeasoning();
+    return (St, Mi, Se);
+}
+
+SoupType GetSoupType()
+{
+    Console.WriteLine("Pick a soup type (soup, stew, gumbo): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "soup" => SoupType.Soup,
+        "stew" => SoupType.Stew,
+        "gumbo" => SoupType.Gumbo,
+    };
+
+}
+
+MainIngredient GetMainIngredient()
+{
+    Console.WriteLine("Pick a Main Ingredient (mushrooms, chicken, carrots, potatoes): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "mushrooms" => MainIngredient.Mushrooms,
+        "chicken" => MainIngredient.Chicken,
+        "carrots" => MainIngredient.Carrots,
+        "potatoes" => MainIngredient.Potatoes,
+    };
+
+}
+Seasoning GetSeasoning()
+{
+    Console.WriteLine("Pick a seasoning (spicy, salty, sweet): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "spicy" => Seasoning.Spicy,
+        "salty" => Seasoning.Salty,
+        "sweet" => Seasoning.Sweet,
+    };
+}
+
+enum SoupType { Soup, Stew, Gumbo }
+enum MainIngredient { Mushrooms, Chicken, Carrots, Potatoes }
+enum Seasoning { Spicy, Salty, Sweet }
