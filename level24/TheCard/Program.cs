12b8faa77526ca08f46fa51e8d2e08afe2a9d329
Carter Warren <cwarren2023@chadwickschool.org>
2022-07-11 12:17:14 -0700

    wip: Add TheCard challenge

---
+++ level24/TheCard/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+enum Color { Red, Green, Blue, Yellow }
+enum Rank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, $, %, ^, &}
---
+++ level24/TheCard/README.md
@@ -0,0 +1,7 @@
+# Questions 
+
+* How do I do the enum with numbers and those symbols??
+
+# Knowledge Check
+
+* 
