9fcc3d1faa4f11f422d12ac77cb688536455d410
Josh Thomas <jthomas2023@chadwickschool.org>
2022-07-12 10:40:27 -0700

    feat: Implement TheCard

--- level24/TheCard/Program.cs
+++ level24/TheCard/Program.cs
@@ -1,5 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// There were too many options for the colors in the previous program.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        foreach (string color in Enum.GetNames(typeof(Color)))
+        {
+            foreach (string rank in Enum.GetNames(typeof(Rank)))
+            {
+                Console.WriteLine($"The {color} {rank}");
+            }
+        }
+    }
+}
 
+public class Card
+{
+    public Rank Rank { get; }
+    public Color Color { get; }
 
-enum Color { Red, Green, Blue, Yellow }
-enum Rank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, $, %, ^, &}
+    public Card(Rank rank, Color color)
+    {
+        Rank = rank;
+        Color = color;
+    }
+}
+
+public enum Color { Red, Green, Blue, Yellow }
+public enum Rank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, DollarSign, Percent, Caret, Ampersand }
