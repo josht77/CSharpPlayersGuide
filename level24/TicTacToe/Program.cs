a7282b76cc76af65a9ff24f38295bb2e7f44cabd
Josh Thomas <jthomas2023@chadwickschool.org>
2022-07-14 10:28:04 -0700

    docs: Bouta mess everything up

--- level24/TicTacToe/Program.cs
+++ level24/TicTacToe/Program.cs
@@ -1,14 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 TicTacToe ttt = new TicTacToe();
-Console.WriteLine(ttt._screenTemplate);
-Console.Clear();
-ttt.board[0] = 'X';
+ttt._board[0] = 'X';
+ttt._board[8] = 'X';
 ttt.playGame();
 public class TicTacToe
 {
     public readonly string _screenTemplate;
-    public char[] board;
+    public char[] _board;
     private char player1;
     private char player2;
     public void playGame() {
@@ -18,17 +17,16 @@ public class TicTacToe
     }
 
     public TicTacToe() {
-        board = new char[9];
+        _board = new char[9]{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
         _screenTemplate = File.ReadAllText("Board.txt");
     }
 
     private void DisplayBoard() {
         string board = _screenTemplate;
-        // for(int i = 0; i < board.Length(); i++) {
-            // replace 'i+1' with board[i]
-            board = board.Replace("1", board[0].ToString());
-            Console.WriteLine(board);
-        // }
+        for(int i = 0; i < this._board.Length; i++) {
+            board = board.Replace($"{i+1}", this._board[i].ToString());
+        }
+        Console.WriteLine(board);
     }
     
 }
