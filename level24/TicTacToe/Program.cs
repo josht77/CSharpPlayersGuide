8c46fc56f74d240ba69d2bef0a649515b77821b4
Josh Thomas <jthomas2023@chadwickschool.org>
2022-07-14 11:39:39 -0700

    feat: Implement TicTacToe

--- level24/TicTacToe/Program.cs
+++ level24/TicTacToe/Program.cs
@@ -1,88 +1,152 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-TicTacToe ttt = new TicTacToe();
-<<<<<<< HEAD
-ttt._board[0] = 'X';
-ttt._board[8] = 'X';
-=======
-Console.Clear();
-ttt.board[0] = 'X';
->>>>>>> 64b60e8cc86047f49bb4f76a55777ab72880a51f
-ttt.playGame();
+TicTacToe ticTacToe = new TicTacToe();
+ticTacToe.playGame();
 public class TicTacToe
 {
     public readonly string _screenTemplate;
     public char[] _board;
-    private char player1;
-    private char player2;
-    private bool isPlayer1Turn = true;
-
-    public void playGame() {
+    private char _player1;
+    private char _player2;
+    private char _winner;
+    private int _numOpenSpaces = 9;
+    private bool _isPlayer1Turn = true;
+    public void playGame()
+    {
         DisplayDirections();
-        getChar();
-        getCurrentTurn();
+        assignPlayers();
+        Console.WriteLine($"Player 1: {_player1}");
+        Console.WriteLine($"Player 2: {_player2}");
         DisplayBoard();
-        getMove();
-        // assignPlayers();
-        
+        char possibleWinner = 'X';
+        while (!hasWon(possibleWinner) && _numOpenSpaces > 0)
+        {
+            getCurrentTurn();
+            getMove();
+            DisplayBoard();
+            possibleWinner = _isPlayer1Turn ? _player2 : _player1;
+        }
+        if (_numOpenSpaces == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("It's a draw!!!");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{_winner} wins!!!!");
+        }
     }
-
-    public TicTacToe() {
-        _board = new char[9]{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    public TicTacToe()
+    {
+        _board = new char[9] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
         _screenTemplate = File.ReadAllText("Board.txt");
     }
-
-    // ?????
-    private void DisplayBoard() {
+    private void DisplayBoard()
+    {
         string board = _screenTemplate;
-        for(int i = 0; i < this._board.Length; i++) {
-            board = board.Replace($"{i+1}", this._board[i].ToString());
+        for (int i = 0; i < this._board.Length; i++)
+        {
+            board = board.Replace($"{i + 1}", this._board[i].ToString());
         }
         Console.WriteLine(board);
     }
-
-
-
-    private void DisplayDirections() {
+    private void DisplayDirections()
+    {
         Console.WriteLine("Directions:");
         Console.WriteLine("Use your number keys(1-9) to select a postion to place your 'X' or 'O'.");
         Console.WriteLine("The numbers coorespond to locations on the board like this:");
-        Console.WriteLine(_screenTemplate);
-        // Console.Clear();
+        Console.WriteLine(_screenTemplate + "\n");
     }
-    // Can we use Readkey or ReadLine? 
-    private void getChar(){
+    private void assignPlayers()
+    {
         Console.WriteLine("What character do you want to play as 'X' or 'O'?");
-        Console.ReadKey();
-        // assign to player1 variable
+        _player1 = char.ToUpper(char.Parse(Console.ReadLine()!));
+        if (_player1 == 'X')
+        {
+            _player2 = 'O';
+        }
+        else
+        {
+            _player2 = 'X';
+        }
     }
-
-    private void getCurrentTurn() {
-        string currentTurn = isPlayer1Turn ? "X" : "O";
+    private char getCurrentTurn()
+    {
+        string currentTurn = _isPlayer1Turn ? _player1.ToString() : _player2.ToString();
         Console.WriteLine($"It is {currentTurn}'s turn.");
-        isPlayer1Turn = !isPlayer1Turn;
+        _isPlayer1Turn = !_isPlayer1Turn;
+        return char.Parse(currentTurn);
     }
-
-    private void getMove() {
+    private void getMove()
+    {
         Console.WriteLine("What square do you want to play in?");
         int location = Convert.ToInt32(Console.ReadLine()!);
-        while(!isValidMove(location)) {
+        while (!isValidMove(location))
+        {
             Console.WriteLine("That square is already full.");
             Console.WriteLine("What square do you want to play in?");
             location = Convert.ToInt32(Console.ReadLine()!);
         }
+        this._board[location - 1] = _isPlayer1Turn ? _player2 : _player1;
+        _numOpenSpaces--;
     }
-
-    // how do I check if that spot is empty?
-    private bool isValidMove(int move) {
-        if(board[move] == ' ') {
+    private bool isValidMove(int move)
+    {
+        if (move > 9 || this._board[move - 1] != ' ')
+        {
+            return false;
+        }
+        return true;
+    }
+    private bool hasWon(char letter)
+    {
+        if (hasWonTopHorizontal(letter) ||
+               hasWonMidHorizontal(letter) ||
+               hasWonBottomHorizontal(letter) ||
+               hasWonLeftVert(letter) ||
+               hasWonMidVert(letter) ||
+               hasWonRightVert(letter) ||
+               hasWonDiagonalLeft(letter) ||
+               hasWonDiagonalRight(letter))
+        {
+            this._winner = letter;
             return true;
         }
-        return false;
+        else
+        {
+            return false;
+        }
     }
-    
-    
-    
+    private bool hasWonTopHorizontal(char letter)
+    {
+        return this._board[0] == letter && this._board[1] == letter && this._board[2] == letter;
+    }
+    private bool hasWonMidHorizontal(char letter)
+    {
+        return this._board[3] == letter && this._board[4] == letter && this._board[5] == letter;
+    }
+    private bool hasWonBottomHorizontal(char letter)
+    {
+        return this._board[6] == letter && this._board[7] == letter && this._board[8] == letter;
+    }
+    private bool hasWonLeftVert(char letter)
+    {
+        return this._board[0] == letter && this._board[3] == letter && this._board[6] == letter;
+    }
+    private bool hasWonMidVert(char letter)
+    {
+        return this._board[1] == letter && this._board[4] == letter && this._board[7] == letter;
+    }
+    private bool hasWonRightVert(char letter)
+    {
+        return this._board[2] == letter && this._board[5] == letter && this._board[8] == letter;
+    }
+    private bool hasWonDiagonalLeft(char letter)
+    {
+        return this._board[0] == letter && this._board[4] == letter && this._board[8] == letter;
+    }
+    private bool hasWonDiagonalRight(char letter)
+    {
+        return this._board[2] == letter && this._board[4] == letter && this._board[6] == letter;
+    }
 }
-
-
--- level24/TicTacToe/README.md
+++ level24/TicTacToe/README.md
@@ -17,6 +17,14 @@ Instance variables:
 | 7 | 8 | 9 |
 -------------
 
+-------------
+| 0 | 1 | 2 |
+|-----------|
+| 3 | 4 | 5 |
+|-----------|
+| 6 | 7 | 8 |
+-------------
+hasWon 
 
 1. Display a tic-tac-toe board with the numbers 
 2. Assigning x's or o's to players 
@@ -32,3 +40,4 @@ Instance variables:
     - ch is the player's letter assignment
     - returns true if they won, false otherwise
 
+QUESTION: Make a for loop for the games. Track stats (counter that says how many times each player has won). Alternate which player goes first each game.
